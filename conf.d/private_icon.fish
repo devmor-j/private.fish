3c167571af66eb097e05a782ff2ffcf403cffb34
Morteza Jamshidi <devmor.j@gmail.com>
2025-08-18 15:16:25 +0000

    add option to clear lines when exiting private mode

--- conf.d/private_icon.fish
+++ conf.d/private_icon.fish
@@ -9,6 +9,10 @@ if not set -q fish_private_icon_show_count
     set -U fish_private_icon_show_count true
 end
 
+if not set -q fish_private_icon_clear_lines
+    set -U fish_private_icon_clear_lines false
+end
+
 # --- Pick a state directory (single path, not a list) ---
 if set -q XDG_RUNTIME_DIR
     set -g fish_private_icon_state "$XDG_RUNTIME_DIR/fish-private-icon"
@@ -28,11 +32,39 @@ if status --is-interactive
         # Register this session PID
         echo $fish_pid >>$fish_private_icon_state/sessions
 
+        # --- Optional: buffer file for clearing lines ---
+        if test $fish_private_icon_clear_lines = true
+            set -g fish_private_icon_buffer "$fish_private_icon_state/private_$fish_pid.log"
+
+            # Prepend each output line to buffer
+            function _fish_private_icon_record_output --on-event fish_prompt
+                if test -z "$_fish_private_icon_recording"
+                    set -g _fish_private_icon_recording 1
+                    # Wrap echo to record lines
+                    function echo --wraps=echo
+                        command echo $argv | tee -a $fish_private_icon_buffer
+                    end
+                end
+            end
+        end
+
         # Clean up on exit
         function _fish_private_icon_cleanup --on-event fish_exit
+            # Remove PID from sessions
             if test -f $fish_private_icon_state/sessions
                 grep -v "^$fish_pid\$" $fish_private_icon_state/sessions >$fish_private_icon_state/sessions.tmp
                 mv $fish_private_icon_state/sessions.tmp $fish_private_icon_state/sessions
+            end
+
+            # Clear only lines written in private mode
+            if test $fish_private_icon_clear_lines = true
+                if test -f $fish_private_icon_buffer
+                    set -l lines (wc -l < $fish_private_icon_buffer)
+                    for i in (seq $lines)
+                        echo -ne "\033[1A\033[2K"
+                    end
+                    rm $fish_private_icon_buffer
+                end
             end
         end
     end
