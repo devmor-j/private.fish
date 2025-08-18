ae3c16432ede32cc4cebc04168e8cbcf39aa868c
Morteza Jamshidi <devmor.j@gmail.com>
2025-08-18 15:00:23 +0000

    fix install bugs

--- conf.d/private_icon.fish
+++ conf.d/private_icon.fish
@@ -9,20 +9,42 @@ if not set -q fish_private_icon_show_count
     set -U fish_private_icon_show_count true
 end
 
+# --- Pick a state directory (single path, not a list) ---
+if set -q XDG_RUNTIME_DIR
+    set -g fish_private_icon_state "$XDG_RUNTIME_DIR/fish-private-icon"
+else if set -q XDG_STATE_HOME
+    set -g fish_private_icon_state "$XDG_STATE_HOME/fish-private-icon"
+else
+    set -g fish_private_icon_state "$HOME/.local/state/fish-private-icon"
+end
+
+mkdir -p $fish_private_icon_state
+
 # --- Detect private session ---
-# Convention: users launch fish with `fish --private` or export FISH_PRIVATE=1
 if status --is-interactive
-    if set -q FISH_PRIVATE
+    if set -q fish_private_mode
         set -g fish_private_icon_active 1
+
+        # Register this session PID
+        echo $fish_pid >>$fish_private_icon_state/sessions
+
+        # Clean up on exit
+        function _fish_private_icon_cleanup --on-event fish_exit
+            if test -f $fish_private_icon_state/sessions
+                grep -v "^$fish_pid\$" $fish_private_icon_state/sessions >$fish_private_icon_state/sessions.tmp
+                mv $fish_private_icon_state/sessions.tmp $fish_private_icon_state/sessions
+            end
+        end
     end
 end
 
 # --- Prompt auto-hook ---
-# Only wrap once to avoid recursion
-if functions -q fish_prompt; and not functions -q _fish_private_icon_original_prompt
-    functions --copy fish_prompt _fish_private_icon_original_prompt
-    function fish_prompt
-        private_icon_prompt
-        _fish_private_icon_original_prompt
+function _fish_private_icon_wrap_prompt --on-event fish_prompt
+    if functions -q fish_prompt; and not functions -q _fish_private_icon_original_prompt
+        functions --copy fish_prompt _fish_private_icon_original_prompt
+        function fish_prompt
+            private_icon_prompt
+            _fish_private_icon_original_prompt
+        end
     end
 end
--- functions/private_icon_count.fish
+++ functions/private_icon_count.fish
@@ -1,5 +1,12 @@
 function private_icon_count --description "Count active private Fish sessions"
-    # Count processes with FISH_PRIVATE exported
-    set -l count (ps -o pid= -o command= -C fish ^/dev/null | grep -c "FISH_PRIVATE")
-    echo $count
+    if not set -q fish_private_icon_state
+        echo 0
+        return
+    end
+    set -l sessions_file "$fish_private_icon_state/sessions"
+    if not test -f $sessions_file
+        echo 0
+        return
+    end
+    wc -l <$sessions_file | string trim
 end
